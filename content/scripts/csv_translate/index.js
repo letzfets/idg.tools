81e0e3596294363e0e7dd0da2ba8cb7fbcf92e37
letzfets <arnold.knott@gmail.com>
2023-05-09 21:14:11 +0200

    doc: adds comments to implement real google interface

--- content/scripts/csv_translate/index.js
+++ content/scripts/csv_translate/index.js
@@ -1,13 +1,17 @@
 import { readFile } from 'fs';
-import translate from 'translate-google'
+//import translate from 'translate-google'
+import translate from 'google-translate'
+// needs pnpm i @google-cloud/translate
+
 
 readFile('test.csv', 'utf8', (err, data) => {
     if (err) throw err
-    console.log(data)
-    console.log('=====================================')
+    //console.log(data)
+    //console.log('=====================================')
     const string = 'Hello world!'
-    translate(string, { to: 'es' })
+    translate(string, { to: 'sv' }, 'translate.google.com')
         .then(res => {
             console.log(res)
         })
 })
+
